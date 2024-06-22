51172ada8fbe1584722ba35737050008fc752ee9
Rowan Murray <rowanomurray@gmail.com>
2024-06-22 15:32:56 +0100

    feat: parse json

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+const filename = "blob.json"
 
 func main() {
-	fmt.Println("Hello world")
+	blobBytes, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var blob map[string]interface{}
+	err = json.Unmarshal(blobBytes, &blob)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("File: %dB\n", len(blobBytes))
+	for key, element := range blob {
+		elementBytes, err := json.Marshal(element)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Printf("- %s: %dB\n", key, len(elementBytes))
+	}
+
 }
