6f10b130f997e9c478d6f704bc43e29e9f487929
Rowan Murray <rowanomurray@gmail.com>
2024-07-05 00:28:49 +0100

    feat: highlight outliers

--- blob.go
+++ blob.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"math"
 	"sort"
+	"strings"
 
+	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/lipgloss/tree"
 )
 
@@ -80,18 +82,22 @@ func (b *Blob) addChildren(cfg Config, jsonBlob interface{}, currentDepth int) e
 	return nil
 }
 
-func (b *Blob) getTree() *tree.Tree {
-	blobString := fmt.Sprintf("%s (%s)", b.name, prettyByteSize(b.bytesCount))
-	t := tree.Root(blobString)
+var green = lipgloss.ANSIColor(2)
+var red = lipgloss.ANSIColor(1)
+var blue = lipgloss.ANSIColor(4)
 
+func (b *Blob) getTree() *tree.Tree {
 	childrenArray := []*Blob{}
 	for _, child := range b.children {
 		childrenArray = append(childrenArray, child)
 	}
 
 	sort.Slice(childrenArray, func(i, j int) bool {
-		return childrenArray[i].bytesCount > childrenArray[j].bytesCount
+		return strings.Compare(childrenArray[i].name, childrenArray[j].name) == -1
 	})
+
+	blobString := fmt.Sprintf("%s (%s)", b.name, prettyByteSize(b.bytesCount))
+	t := tree.Root(blobString).ItemStyleFunc(getItemStyleFunc(childrenArray))
 
 	for _, child := range childrenArray {
 		t.Child(child.getTree())
@@ -108,4 +114,50 @@ func prettyByteSize(b int) string {
 		bf /= 1024.0
 	}
 	return fmt.Sprintf("%.1fTB", bf)
+}
+
+func getItemStyleFunc(children []*Blob) tree.StyleFunc {
+	lowerBound, upperBound := getOutlierBounds(children)
+	return func(_ tree.Children, i int) lipgloss.Style {
+		if i < 0 {
+			return lipgloss.NewStyle()
+		}
+
+		itemBytes := children[i].bytesCount
+		if itemBytes > upperBound {
+			return lipgloss.NewStyle().Foreground(red)
+		} else if itemBytes < lowerBound {
+			return lipgloss.NewStyle().Foreground(green)
+		}
+
+		return lipgloss.NewStyle().Foreground(blue)
+	}
+}
+
+func getOutlierBounds(children []*Blob) (int, int) {
+	values := []float64{}
+	for _, child := range children {
+		values = append(values, float64(child.bytesCount))
+	}
+
+	nOfValues := float64(len(values))
+	sumOfValues := 0.0
+	for _, value := range values {
+		sumOfValues += value
+	}
+
+	mean := sumOfValues / nOfValues
+
+	sumOfSquaresOfDiffToMean := 0.0
+	for _, value := range values {
+		sumOfSquaresOfDiffToMean += math.Pow(value-mean, 2)
+	}
+
+	variance := sumOfSquaresOfDiffToMean / nOfValues
+	standardDeviation := math.Sqrt(variance)
+
+	lowerBound := int(mean - 3*standardDeviation)
+	upperBound := int(mean + 3*standardDeviation)
+
+	return lowerBound, upperBound
 }
